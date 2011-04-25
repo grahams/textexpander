65b1c47680ae8b8dc2865889c204e8dfef2408c7
Sean Graham <github@sean-graham.com>
2011-04-25 10:59:16 -0400

    replaced call to print to remove newline after shorturl

--- googl-shortener.py
+++ googl-shortener.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
+import sys
 import googl # http://code.google.com/p/python-googl/
 
 key = "myKeyFromGoogleApiConsole"
 
 api = googl.Googl(key)
 link = api.shorten("%clipboard")
-print(link['id'])
-
+sys.stdout.write(link['id'])

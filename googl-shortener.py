cfbf066eb3fbaa353829b96d9eddf66fe7a2bfa1
Sean Graham <github@sean-graham.com>
2014-03-06 10:42:02 -0500

    updated to use official google python API lib

--- googl-shortener.py
+++ googl-shortener.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python
 
+# This snippet requires:
+# 1) The official [Google API Client Libraries: 
+#       https://code.google.com/p/google-api-python-client/
+#       (pip install --upgrade google-api-python-client)
+#
+# 2) A simple API key from Google: 
+#       https://developers.google.com/console/help/#generatingdevkeys
+#
+
 import sys
-import googl # http://code.google.com/p/python-googl/
+from apiclient.discovery import build 
 
 key = "myKeyFromGoogleApiConsole"
 
-api = googl.Googl(key)
-link = api.shorten("%clipboard")
-sys.stdout.write(link['id'])
+service = build('urlshortener', 'v1', developerKey=key)
+
+url = service.url()
+
+# Create a shortened URL by inserting the URL into the url collection.
+body = {'longUrl': '%clipboard' }
+resp = url.insert(body=body).execute()
+
+sys.stdout.write(resp['id'])
